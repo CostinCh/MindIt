a112468c0a8c09c4d68c81f59df2fd64dfbc24bb
CostinCh <costin.ch@gmail.com>
2016-12-11 17:16:06 +0200

    Update InsertBrands.sql

--- InsertBrands.sql
+++ InsertBrands.sql
@@ -1,11 +1,11 @@
 use [ItemCatalogue]
 go
 declare @i int=0
-	, @iString varchar(5)=''
+	, @iString varchar(2)=''
 
 while @i<10
 begin 
-	set @iString=cast(@i as varchar(5))
+	set @iString=cast(@i as varchar(2))
 
 	insert into [dbo].[IC_BRAND]
 	values ('Brand'+@iString, 'Brand '+@iString+' description')
